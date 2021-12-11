c0805e9506e5871064f06df75730da095d09a42d
BenderKate <afonia96@gmail.com>
2021-12-11 18:01:42 +0300

    Changed task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,28 @@
+"use strict"
+
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let D = (b * b) - (4 * a * c);
+
+  if (D < 0) {
+    arr = [];
+  } else if (D = 0) {
+    arr = [-b / (2 * a)];
+  } else if (D > 0) {
+    let x1 = (-b + Math.sqrt(D) ) / (2 * a);
+    let x2 = (-b - Math.sqrt(D) ) / (2 * a);
+    arr = [x1, x2];
+  } 
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
+
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
